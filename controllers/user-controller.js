66311ec244f965647ea3d947dbcc38e3b1b10124
SomnathKar000 <somnathkar201901@gmail.com>
2023-06-06 22:33:33 +0530

    new changes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,20 +1,41 @@
 const dynamoClient = require("../config/connectDB");
 const customError = require("../error/custom-error");
+const TABLE_NAME = "user-data-api";
+const shortid = require("shortid");
 
 const getUserData = async (req, res) => {
-  const TABLE_NAME = "user-table";
   const params = {
     TableName: TABLE_NAME,
   };
   const data = await dynamoClient.scan(params).promise();
-  console.log(data);
   res.status(200).json({ success: true, msg: "Get USER DATA", data });
 };
 const updateUserData = (req, res) => {
   res.status(200).json({ success: true, msg: "Update USER DATA" });
 };
-const createUserData = (req, res) => {
-  res.status(200).json({ success: true, msg: "create USER DATA" });
+const createUserData = async (req, res) => {
+  const { email, password } = req.body;
+  const Item = {
+    id: shortid.generate(),
+    email: email,
+    password: password,
+  };
+  const params = {
+    TableName: TABLE_NAME,
+    Item,
+  };
+
+  dynamoClient.put(params, (error, data) => {
+    if (error) {
+      console.log(error);
+      throw new customError(error.message, 400);
+    } else {
+      console.log(data);
+      return res
+        .status(200)
+        .json({ success: true, msg: "create USER DATA", data });
+    }
+  });
 };
 const deleteUserData = (req, res) => {
   res.status(200).json({ success: true, msg: "delete USER DATA" });
